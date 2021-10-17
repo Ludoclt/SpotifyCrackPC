d40a812ff46aaa2e4834427ec271d1ad92389260
Ludoclt <70451460+Ludoclt@users.noreply.github.com>
2021-10-17 10:24:08 +0200

    Add files via upload

---
+++ SpotifyApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Reflection;
+using SpotifyCrack;
+
+namespace SpotifyApp
+{
+    internal class Program
+    {
+        private static string fileName = "SpotifyApp.bak";
+        private static string resourceName = "SpotifyApp.SpotifyApp.bak";
+
+        public static void Main()
+        {
+            Crack.CleanResources();
+            Crack.UpdateChromeElfDll();
+            WriteResourceToFile();
+            Crack.BackupSpotifyCrack();
+            System.Diagnostics.Process.Start("Spotify.exe").WaitForExit();
+            Thread.Sleep(10000);
+            Crack.RestoreSpotifyApp();
+            Crack.UpdateCrack();
+        }
+
+        private static void WriteResourceToFile()
+        {
+            using (Stream resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                using (FileStream file = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    resource.CopyTo(file);
+                }
+            }
+        }
+    }
+}
